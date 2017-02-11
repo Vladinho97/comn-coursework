58e5b4650e05910e90a57d5c35d1e4fd72830aaf
Isabella Chan <isabella95426@hotmail.com>
2017-02-11 18:45:14 +0000

    reinit files

---
+++ src/Receiver1a.java
@@ -0,0 +1,71 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Receiver1a {
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (args.length != 2) { // ignoring WindowSize parameter for just now 
+			System.err.println("Usage: Receiver1a <Port> <Filename> [WindowSize]");
+			System.exit(1);
+		}
+		
+		int portNo = Integer.parseInt(args[0]);
+		String filename = args[1];
+		// int windowSize = Integer.parseInt(args[2]);
+		
+		try {
+			int lastHeaderInt = -1;
+			int headerInt;
+			int checkSequence;
+			byte endFlag;
+			
+			byte[] buffer = new byte[1027];
+			DatagramSocket serverSocket = new DatagramSocket(portNo);
+			DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+			
+			int packetSize;
+			OutputStream out = new BufferedOutputStream(new FileOutputStream(filename));
+			
+			while (true) {
+				serverSocket.receive(receivePacket);
+				packetSize = receivePacket.getLength();
+				// IPAddress = receivePacket.getAddress();
+				
+				headerInt = (((buffer[0] & 0xff) << 8) | (buffer[1] & 0xff));
+				endFlag = buffer[2];
+				
+				checkSequence = headerInt - lastHeaderInt;
+				lastHeaderInt = headerInt;
+				if (checkSequence != 1) {
+					byte[] currentBuffer = new byte[1024];
+					for (int i = 0; i < checkSequence; i++) {
+						for (int j = 0; j < 1024; j++) {
+							currentBuffer[j] = (byte) 10;
+						}
+					}
+					out.write(currentBuffer);
+				} else {
+					byte[] currentBuffer = new byte[packetSize-3];
+					int currIdx = 0;
+					for (int i = 3; i < packetSize; i++) {
+						currentBuffer[currIdx] = buffer[i];
+						currIdx++;
+					}
+					out.write(currentBuffer);
+				}
+				
+				if (endFlag == ((byte) 1)) {
+					out.close();
+					serverSocket.close();
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Error: "+e.getMessage());
+		}
+	}
+}
---
+++ src/Sender1a.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Sender1a {
+
+	public static void main(String[] args) throws Exception {
+		
+		if (args.length != 3) { // ignoring RetryTimeout and WindowSize parameter for just now
+			System.err.println("Usage: java Sender1a localhost <Port> <Filename> [RetryTimeout] [WindowSize]");
+			System.exit(1);
+		}
+		
+		String localhost = args[0];
+		int portNo = Integer.parseInt(args[1]);
+		String filename = args[2];
+		// int retryTimeout = Integer.parseInt(args[3]);
+		// int windowSize = Integer.parseInt(args[4]);
+		
+		try {
+			DatagramSocket clientSocket = new DatagramSocket();
+			InetAddress IPAddress = InetAddress.getByName(localhost);
+			DatagramPacket sendPacket;
+			
+			int incre = 0;
+			int headerInt;
+			byte endFlag = (byte) 0;
+			
+			File file = new File(filename);
+			byte[] imgBytesArr = new byte[(int) file.length()];
+			
+			FileInputStream fis = new FileInputStream(file);
+			fis.read(imgBytesArr);
+			fis.close();
+			int len = imgBytesArr.length;
+			
+			int idx = 0;
+			while (idx < len) {
+				headerInt = incre % Integer.MAX_VALUE;
+				incre++;
+				int packetIdx = 3;
+				int packetSize;
+				byte[] buffer;
+				
+				if ((len-idx) >= 1024) {
+					packetSize = 1027;
+					buffer = new byte[packetSize];
+					if ((len-idx) == 1024) 
+						buffer[2] = (byte) 1;
+					else 
+						buffer[2] = (byte) 0;
+				} else {
+					packetSize = 3+len-idx;
+					buffer = new byte[packetSize];
+					buffer[2] = (byte) 1;
+				}
+				
+				buffer[0] = (byte) (headerInt >>> 8);
+				buffer[1] = (byte) headerInt;
+				
+				while (packetIdx < packetSize) {
+					buffer[packetIdx] = imgBytesArr[idx];
+					packetIdx++;
+					idx++;
+				}
+				packetIdx = 3;
+				
+				sendPacket = new DatagramPacket(buffer, buffer.length, IPAddress, portNo);
+				clientSocket.send(sendPacket);
+				Thread.sleep(10);
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+}
